78394d5ffa4f6743f7a0012867f2db68651f22c7
Diana <efremova.diana00.00@mail.rb>
2023-05-14 22:23:27 +0300

    version2

--- sh.rb
+++ sh.rb
@@ -1,1 +1,18 @@
-puts 'hi'
+require 'digest'
+
+puts 'Введите слово или фразу для шифрования:'
+string = STDIN.gets.chomp
+
+puts 'Каким способом зашифровать:
+1. MD5
+2. SHA1'
+us = STDIN.gets.to_i
+
+puts 'Вот что получилось:'
+
+case us
+when 1
+  puts Digest::MD5.hexdigest(string)
+when 2
+  puts Digest::SHA1.hexdigest(string)
+end
