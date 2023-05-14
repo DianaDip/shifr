371aab4eaaeae02fc01cb2b45d1d12c511cd0220
Diana <efremova.diana00.00@mail.rb>
2023-05-14 22:57:30 +0300

    commit message

---
+++ sh.rb
@@ -0,0 +1,18 @@
+require 'digest'
+
+puts 'Введите слово или фразу для шифрования:'
+string = STDIN.gets.chomp
+
+puts 'Каким способом зашифровать:
+1. MD5
+2. SHA1'
+us = STDIN.gets.to_i
+
+puts 'Вот что получилось:'
+
+case us
+when 1
+  puts Digest::MD5.hexdigest(string)
+when 2
+  puts Digest::SHA1.hexdigest(string)
+end
